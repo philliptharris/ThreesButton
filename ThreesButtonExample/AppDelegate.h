1cbf44dddb2836384d772c2bb2f103d7ce391ac4
Phillip Harris <phillip.harris@atlanticbt.com>
2014-05-13 14:41:46 -0400

    Initial Commit

---
+++ ThreesButtonExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ThreesButtonExample
+//
+//  Created by Phillip Harris on 5/13/14.
+//  Copyright (c) 2014 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ThreesButtonExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ThreesButtonExample
+//
+//  Created by Phillip Harris on 5/13/14.
+//  Copyright (c) 2014 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
